1128bd30b4d2b4dabab3044f0a3c86fa77ee3582
Maximiliano José Sorribas <mjsorribas@gmail.com>
2023-01-23 14:14:24 -0300

    feat: update  ui of data settings page (#24)

--- src/features/services/components/data-settings.tsx
+++ src/features/services/components/data-settings.tsx
@@ -1,26 +1,66 @@
-import { Box, Button, Flex, useDisclosure, Center } from "@liftedinit/ui";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Table,
+  Tbody,
+  Thead,
+  Tr,
+  Td,
+  Th,
+  useDisclosure,
+} from "@liftedinit/ui";
 import { PutValueModal } from "../components";
 import { useAccountsStore } from "features/accounts";
 import { ANON_IDENTITY } from "@liftedinit/many-js";
 
+interface KVData {
+  key: string;
+  value: string;
+  tag: string;
+}
+
+// eslint-disable-next-line
+function KVDataRow({ key, value, tag }: KVData) {
+  return (
+    <Tr key={key}>
+      <Td>{key}</Td>
+      <Td>{value}</Td>
+      <Td>{tag}</Td>
+    </Tr>
+  );
+}
+
 export function DataSettings() {
   const account = useAccountsStore((s) => s.byId.get(s.activeId));
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <>
       <Box p={6} bg="white" mt={9} boxShadow="xl">
-        <Center>
-          <pre>PLACEHOLDER FOR liftedinit/roadmap#17</pre>
-        </Center>
-      </Box>
-
-      {account?.address !== ANON_IDENTITY && (
+        <Heading size="md" mb={6}>
+          All Key/Value Store
+        </Heading>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Key</Th>
+              <Th>Value</Th>
+              <Th>Tag</Th>
+              <Th>Edit</Th>
+            </Tr>
+          </Thead>
+          <Tbody></Tbody>
+        </Table>
+        {account?.address !== ANON_IDENTITY && (
         <Flex mt={9} justifyContent="flex-end" w="full">
           <Button width={{ base: "full", md: "auto" }} onClick={onOpen}>
             Create New Key
           </Button>
         </Flex>
       )}
+      </Box>
       {isOpen && <PutValueModal isOpen={isOpen} onClose={onClose} />}
     </>
   );
