1f564a094a19f5ce04663b1102e929d7492e9a8e
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2023-07-14 11:05:28 -0400

    feat: also list immutable keys (#55)

--- src/pages/services/data/data.tsx
+++ src/pages/services/data/data.tsx
@@ -1,33 +1,34 @@
-import { ANON_IDENTITY } from "@liftedinit/many-js";
-import { Box, Button, Flex, Heading, useDisclosure } from "@liftedinit/ui";
-import { NeighborhoodContext } from "api/neighborhoods";
+import {ANON_IDENTITY} from "@liftedinit/many-js";
+import {Box, Button, Flex, Heading, useDisclosure} from "@liftedinit/ui";
+import {NeighborhoodContext} from "api/neighborhoods";
 import {
   combineData,
   useGetValues,
   useListKeys,
   useQueryValues,
 } from "api/services";
-import { useAccountsStore } from "features/accounts";
-import { useContext, useState } from "react";
-import { Breadcrumbs } from "../breadcrumbs";
-import { DataTable } from "./data-table";
-import { PutValueModal } from "./put-value-modal";
+import {useAccountsStore} from "features/accounts";
+import {useContext, useState} from "react";
+import {Breadcrumbs} from "../breadcrumbs";
+import {DataTable} from "./data-table";
+import {PutValueModal} from "./put-value-modal";
 
 export function Data() {
   const account = useAccountsStore((s) => s.byId.get(s.activeId));
   const neighborhood = useContext(NeighborhoodContext);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [keyvalue, setKeyvalue] = useState({ key: "", value: "" });
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  const [keyvalue, setKeyvalue] = useState({key: "", value: ""});
 
-  const { data: keys } = useListKeys(neighborhood, account?.address.toString());
-  const values = useGetValues(neighborhood, keys?.keys);
-  const queries = useQueryValues(neighborhood, keys?.keys);
+  const list = useListKeys(neighborhood, account?.address.toString());
+  const all_keys = list.flatMap(item => item.data?.keys || []);
+  const values = useGetValues(neighborhood, all_keys);
+  const queries = useQueryValues(neighborhood, all_keys);
   const data = combineData([...values, ...queries]);
 
   return (
     <Box p={6}>
       <Heading>Data</Heading>
-      <Breadcrumbs title="Data" />
+      <Breadcrumbs title="Data"/>
 
       <DataTable
         data={data}
@@ -38,9 +39,9 @@ export function Data() {
       {account?.address !== ANON_IDENTITY && (
         <Flex mt={9} justifyContent="flex-end" w="full">
           <Button
-            width={{ base: "full", md: "auto" }}
+            width={{base: "full", md: "auto"}}
             onClick={() => {
-              setKeyvalue({ key: "", value: "" });
+              setKeyvalue({key: "", value: ""});
               onOpen();
             }}
           >
