e145da665f3cc2e5b858c5713f33a57aab061fb4
Stanley Jones <stanley@liftedinit.org>
2023-07-14 06:57:10 -0700

    build: Upgrade Node to v18.16.1 (#57)

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,28 @@
+const webpack = require("webpack");
+
 module.exports = function override(webpackConfig) {
+  webpackConfig.resolve.fallback = {
+    buffer: require.resolve("buffer"),
+    crypto: require.resolve("crypto-browserify"),
+    http: require.resolve("stream-http"),
+    https: require.resolve("https-browserify"),
+    process: require.resolve("process/browser"),
+    stream: require.resolve("stream-browserify"),
+    zlib: require.resolve("browserify-zlib"),
+  };
+  webpackConfig.plugins.push(
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    })
+  );
   webpackConfig.module.rules.push({
     test: /\.mjs$/,
     include: /node_modules/,
     type: "javascript/auto",
+    resolve: {
+      fullySpecified: false,
+    },
   });
 
   return webpackConfig;
